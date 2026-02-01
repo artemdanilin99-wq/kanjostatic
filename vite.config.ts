da392a8bfbf5f02b703432d9a8b830f8267dee32
artemdanilin99-wq <artem.danilin99@gmail.com>
2026-02-01 22:33:21 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
+    emptyOutDir: true
   }
 });
