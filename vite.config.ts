bf1bede696a79d7dc052ab6bf2fbecb0a59e2b81
artemdanilin99-wq <artem.danilin99@gmail.com>
2026-02-01 21:50:20 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    // Явно указываем Vite, какие расширения искать
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
+  }
 });
