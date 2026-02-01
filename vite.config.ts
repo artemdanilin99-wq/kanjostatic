31a448bf4d080ad80daf1d2816e3cc77827ca6bc
artemdanilin99-wq <artem.danilin99@gmail.com>
2026-02-01 22:19:19 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
-    host: true
+    strictPort: true,
   }
 });
